0ba216fc90c704b48f79b0d8ac8f5c990850a5ad
Maja <maja.bloom@outlook.com>
2026-05-11 08:19:36 +0200

    Add Spring and refactored to use Dependency Injection: Add Spring dependency to pom.xml. Create AppConfig. Add @Service and @Component to classes so Spring handles them instead of using 'new'

--- src/main/java/se/lexicon/AppConfig.java
+++ src/main/java/se/lexicon/AppConfig.java
@@ -1,4 +1,13 @@
 package se.lexicon;
 
+// Import what I need from Spring
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// Tell Spring that the class contains settings for the project
+@Configuration
+// Tells Spring to look through all my golders for @Component and @Service
+@ComponentScan("se.lexicon")
 public class AppConfig {
+
 }
